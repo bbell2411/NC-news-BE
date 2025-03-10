9c8102a814ffd6d041feadfff98dac418c1b930b
bbell2411 <belkise.elmm2003@gmail.com>
2025-03-10 11:37:09 +0000

    added files such as app.js and listen.js & mvc files in order to make a test for task 2 in sprint

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,7 +1,19 @@
 const endpointsJson = require("../endpoints.json");
-/* Set up your test imports here */
+const request = require('supertest')
+const seed = require('../db/seeds/seed')
+const data = require('../db/data/test-data')
+const app = require('../app')
+const db = require('../db/connection')
 
-/* Set up your beforeEach & afterAll functions here */
+
+beforeEach(() => {
+  return seed(data);
+});
+
+afterAll(() => {
+  return db.end();
+});
+
 
 xdescribe("GET /api", () => {
   test("200: Responds with an object detailing the documentation for each endpoint", () => {
@@ -13,3 +25,21 @@ xdescribe("GET /api", () => {
       });
   });
 });
+
+
+describe('GET /api/topics', () => {
+  test('200: gets topics', () => {
+    return request(app)
+      .get('/api/topics')
+      .expect(200)
+      .then(({ body }) => {
+        body.topics.forEach((topic) => {
+          const { description, slug, img_url } = topic
+          expect(typeof description).toBe('string')
+          expect(typeof slug).toBe('string')
+          expect(typeof img_url).toBe('string')
+        })
+      })
+
+  })
+})
